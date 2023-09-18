317316208031f0e09c388ae41a95bfa9481ec1ad
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-09-18 22:54:24 +0200

    feat: display hough lines

--- 01_Detecting_lane_lines/lane_line_detection.py
+++ 01_Detecting_lane_lines/lane_line_detection.py
@@ -41,6 +41,27 @@ def region_of_interest(image: np.ndarray) -> np.ndarray:
     return masked_image
 
 
+def display_lines(masked_image: np.ndarray, hough_lines: np.ndarray) -> np.ndarray:
+    """
+    This function displays the lines on the input image
+    :param masked_image: input image
+    :param hough_lines: lines to be displayed
+    :return: None
+    """
+    # Create a copy of the image
+    line_img = np.zeros_like(masked_image)
+    # Check if the lines are not empty
+    if hough_lines is not None:
+        # Iterate over all the lines
+        for line in hough_lines:
+            # Reshape the line
+            x1, y1, x2, y2 = line.reshape(4)
+            # Draw the line
+            cv2.line(line_img, (x1, y1), (x2, y2), (255, 0, 0), 10)
+
+    return line_img
+
+
 # Load image
 image = cv2.imread('images/road.jpg')
 # Create a copy of the image
@@ -49,6 +70,10 @@ lane_image = np.copy(image)
 canny_image = canny(lane_image)
 # Apply the region of interest
 cropped_image = region_of_interest(canny_image)
+# Calculate the Hough lines
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi / 180, 120, np.array([]), minLineLength=40, maxLineGap=20)
+# Display the lines on the image
+line_image = display_lines(lane_image, lines)
 
-cv2.imshow('result', cropped_image)
+cv2.imshow('result', line_image)
 cv2.waitKey(0)
