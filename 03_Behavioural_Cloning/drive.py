3145f3c4a475d95f4f704fc2e8401ed7e1f850c7
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-10-10 17:55:22 +0200

    Add: image preprocess

--- 03_Behavioural_Cloning/drive.py
+++ 03_Behavioural_Cloning/drive.py
@@ -11,6 +11,19 @@ import cv2
 sio = socketio.Server()
 
 app = Flask(__name__)  # '__main__' name
+speed_limit = 10
+
+
+def img_preprocess(img):
+    img = img[60:135, :, :]  # shortening height of image [height, width, layer]
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)  # image space used for training NVIDIA neural model
+    img = cv2.GaussianBlur(img, (3, 3), 0)  # smoothening image technique
+    img = cv2.resize(img, (200, 66))  # resizing image as per specifications of NVIDIA neural model
+    img = img / 255  # normalizing image (reduce variance btw image data without visual impact)
+    return img
+
+
+
 
 
 if __name__ == '__main__':
