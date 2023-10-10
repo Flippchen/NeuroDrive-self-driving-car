d4d977f12dd7bc02bd18b5e731bb7661eadfac7d
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-10-10 17:39:22 +0200

    Add: Flask socket

--- 03_Behavioural_Cloning/drive.py
+++ 03_Behavioural_Cloning/drive.py
@@ -1,1 +1,19 @@
+import socketio
+import eventlet
+from flask import Flask
+import numpy as np
+from keras.models import load_model
+import base64
+from io import BytesIO
+from PIL import Image
+import cv2
 
+sio = socketio.Server()
+
+app = Flask(__name__)  # '__main__' name
+
+
+if __name__ == '__main__':
+    model = load_model('self-driving-model.h5')
+    app = socketio.Middleware(sio, app)
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
